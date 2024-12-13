a22fe9bec85b60f4573415fcf300797a414975b6
Galib1710 <md.asrafulgalib00@gmail.com>
2024-12-13 06:31:10 +0000

    changed vite config file to setup npm run build

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+   root: './', // Ensure this is pointing to your project root
+  build: {
+    rollupOptions: {
+      input: './public/Homepage.html' // Explicitly set the entry HTML file
+    }
+  },
+});
